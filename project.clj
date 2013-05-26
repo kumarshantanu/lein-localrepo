cb8da7ef1677daebc15d192a2e707f393e46a14c
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-05-26 15:36:25 +0530

    add tools.cli dependency, bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-localrepo "0.4.1"
+(defproject lein-localrepo "0.5.0-SNAPSHOT"
   :description "Leiningen local repository plugin"
   :url "https://github.com/kumarshantanu/lein-localrepo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;;:dev-dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/tools.cli "0.2.2"]]
   :eval-in-leiningen true)
